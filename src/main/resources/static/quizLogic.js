4cfb139ba3de35101082bcafaebf0d8842026f8f
KennyK-N <kennykwan901@gmail.com>
2024-01-26 16:44:48 -0800

    updated js

--- src/main/resources/static/quizLogic.js
+++ src/main/resources/static/quizLogic.js
@@ -1,7 +1,22 @@
 var Index = 1;
+var score = 0;
 var submit_button = document.querySelector("button[id=Finish_button]");
 submit_button.addEventListener("click", submit);
-var answer = [];
+var answer = [
+  "website",
+  "1",
+  "web",
+  "w32b",
+  "wbx",
+  "2",
+  ["up"],
+  ["salmon", "up"],
+  "3",
+  "4",
+  "mxb",
+  "1",
+];
+//NOTE CHECKBOX ANSWER MUST BE IN ORDER
 var button_disable = document.querySelectorAll("button");
 var multiple_choice_usr_ans = document.querySelectorAll(
   "input[class='multiple_choice']"
@@ -45,9 +60,73 @@ function showDivs(n) {
   currentslide.style.display = "flex";
 }
 
+//change this A FUNCTION to check correct answer when user press nextt
+// Provide feedback for each answer selected (e.g., highlighting the user's answer and the correct answer).
+// WHEN USER PRESS NEXT IT AUTO EVALUATE
+//PROB DONT NEED A LOOP BUT WILL PROB NEED ARRAY TO KEEP TRACK OF WHATS RIGHT OR WRONG FOR END RESULT
 function submit(e) {
   e.preventDefault();
-  alert("done_remove later");
+  var current_type;
+  var currentslide;
+  var current_slide_multi;
+  var matching_index = 0;
+  var slide_array = document.getElementsByClassName("quiz_slides");
+  for (var i = 0; i < 12; i++) {
+    if (slide_array[i + 1].querySelectorAll("input").length === 1) {
+      currentslide = slide_array[i + 1].querySelectorAll("input");
+      current_type = currentslide[0].type;
+    } else {
+      current_slide_multi = slide_array[i + 1].querySelectorAll("input");
+      current_type = current_slide_multi[0].type;
+    }
+    switch (current_type) {
+      case "text":
+        if (currentslide[0].value === answer[i]) {
+          score++;
+          console.log("work");
+        } else {
+          //display error message and correct answer
+          console.log("No work");
+        }
+        break;
+      case "radio":
+        for (var j = 0; j < 4; j++) {
+          if (
+            current_slide_multi[j].checked === true &&
+            current_slide_multi[j].value === answer[i]
+          ) {
+            score++;
+          } else {
+            //display error message and correct answer
+          }
+        }
+        break;
+      case "checkbox":
+        for (var j = 0; j < 4; j++) {
+          if (current_slide_multi[j].checked === true) {
+            if (
+              matching_index < answer[i].length &&
+              current_slide_multi[j].value === answer[i][matching_index]
+            ) {
+              matching_index++;
+            } else {
+              //THERES AN ERROR IN WHICH IF U SELECT ALL OF THEM IT WILL STILL BE CORRECT
+              //this is potential fix but not efficient
+              matching_index--;
+              //display error message and correct answer
+            }
+          }
+        }
+        if (matching_index === answer[i].length) {
+          score++;
+        }
+        matching_index = 0;
+        break;
+      default:
+        break;
+    }
+  }
+  alert("Your score " + score + " done_remove later");
 }
 
 //test
